10d3f807070e4be71e7ac275a925d8017d666425
Arunabh-P <arunabh1995@gmail.com>
2023-02-16 20:44:10 +0530

    frontend-user : header starts

--- frontend-user/src/components/Header.js
+++ frontend-user/src/components/Header.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free shipping over 1000 & Free returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline :{' '}
+                <a href="tel:+91 8714368699" className="text-white">
+                  +91 8714368699
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
